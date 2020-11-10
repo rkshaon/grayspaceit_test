1d2a601aaf96e7748823ddeceadd4df7c2420e6a
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2020-11-10 23:31:27 +0600

    phase 2 - fetch all post and displayed

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import requests
 
 # POSTS VIEW ENDPOINT
 @login_required(login_url='login')
 def posts(request):
-    return render(request, 'blog-listing.html')
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url)
+    post_data = response.json()
+    context = {
+        'posts': post_data,
+    }
+    return render(request, 'blog-listing.html', context)
 
 # POST DETAILS VIEW ENDPOINT
 @login_required(login_url='login')
